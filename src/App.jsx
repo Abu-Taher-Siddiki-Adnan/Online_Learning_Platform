def67ea7c00709fce9a49996a14d88793a9accf2
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2025-12-05 23:22:48 +0600

    Learnify Created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Assalamu Alaikum</h1>
+      <h1>Welcome to Learnify</h1>
+      <p>Country's number one E-Learning Platform</p>
+    </div>
+  )
+}
+
+export default App
